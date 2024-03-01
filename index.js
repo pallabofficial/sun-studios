78627cfe9db43928a200212730dc4e009f8ecd01
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-01 16:42:52 +0530

    fixed the darkmode

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 const menu = document.querySelector("#nav-menu-div");
 const navbar = document.querySelector("nav");
 const navItems = document.querySelector("#expand-nav-menu")
+const darkScreen = document.querySelector("#dark-screen")
 let flag = false;
 menu.addEventListener("click", (event)=>{
     if (flag===false){
         navItems.style.top = "15vh";
+        darkScreen.style.top = "15vh";
         flag=true;
+        darkScreen.style.transitionDuration = "1s"
     }
     else{
         navItems.style.top = "-100%";
+        darkScreen.style.top = "-100%";
         flag=false;
+        darkScreen.style.transitionDuration = "0.2s"
     }
     
 })
