742e002c5814376f8e8851468896665b2b151438
pallabofficial <officialpallabmondal000@gmail.com>
2024-03-01 13:45:48 +0530

    modified the dark mode

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 const menu = document.querySelector("#nav-menu-div");
 const navbar = document.querySelector("nav");
 const navItems = document.querySelector("#expand-nav-menu")
+const darkMode = document.querySelector("#dark-mode")
 let flag = false;
 menu.addEventListener("click", (event)=>{
     if (flag===false){
         navItems.style.top = "15vh";
+        darkMode.style.top="15vh";
         flag=true;
     }
     else{
         navItems.style.top = "-100%";
+        darkMode.style.top="-100%";
         flag=false;
     }
     
